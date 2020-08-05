29438579f3659cdaf71570fa69ff9b45ece35c45
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-05 14:59:24 +0800

    Create APU.sh

---
+++ APU.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+wget http://update.aegis.aliyun.com/download/uninstall.sh
+
+chmod +x uninstall.sh
+
+sudo ./uninstall.sh
+
+wget http://update.aegis.aliyun.com/download/quartz_uninstall.sh
+
+chmod +x quartz_uninstall.sh
+
+./quartz_uninstall.sh
+
+pkill aliyun-service
+
+rm -fr /etc/init.d/agentwatch /usr/sbin/aliyun-service
+
+rm -rf /usr/local/aegis*
+
+iptables -I INPUT -s 140.205.201.0/28 -j DROP
+
+iptables -I INPUT -s 140.205.201.16/29 -j DROP
+
+iptables -I INPUT -s 140.205.201.32/28 -j DROP
+
+iptables -I INPUT -s 140.205.225.192/29 -j DROP
+
+iptables -I INPUT -s 140.205.225.200/30 -j DROP
+
+iptables -I INPUT -s 140.205.225.184/29 -j DROP
+
+iptables -I INPUT -s 140.205.225.183/32 -j DROP
+
+iptables -I INPUT -s 140.205.225.206/32 -j DROP
+
+iptables -I INPUT -s 140.205.225.205/32 -j DROP
+
+/usr/local/cloudmonitor/wrapper/bin/cloudmonitor.sh stop
+
+/usr/local/cloudmonitor/wrapper/bin/cloudmonitor.sh remove
+
+rm -rf /usr/local/cloudmonitor
+
