7670f819dfff8194ed40cb4cb4d76049a9ad471d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-10-11 11:05:48 +0800

    Create TPU.sh

---
+++ TPU.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo 正在执行. . .
+
+echo > /var/spool/cron/root
+
+crontab -l
+
+/usr/local/qcloud/stargate/admin/uninstall.sh
+
+/usr/local/qcloud/YunJing/uninst.sh
+
+/usr/local/qcloud/monitor/barad/admin/uninstall.sh
+
+rm -rf /usr/local/qcloud
+
+ps -A | grep agent
+
+echo 执行成功!
